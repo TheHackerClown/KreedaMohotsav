abdde6858f559acd082bb607d9563c6280c69161
TheHackerClown <dhruvprataps14@gmail.com>
2026-01-31 18:50:31 +0530

    added form

--- components/DAY2REG.tsx
+++ components/DAY2REG.tsx
@@ -1,10 +1,11 @@
 
 export default function Day2Button() {
-  return (<button
+  return (<a
+          href="https://forms.gle/SRHKWwusyipz1X5j6"
           className="
             relative px-3 sm:px-6 py-1.5 sm:py-3 font-mono text-xs sm:text-base font-bold
             transition-all duration-150 ease-in-out
-            bg-white text-black border-black
+            bg-red-500 text-white border-red-950
             border-2 sm:border-4
             shadow-[3px_3px_0px_0px_rgba(0,0,0,0.8)] sm:shadow-[4px_4px_0px_0px_rgba(0,0,0,0.8)]
             hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,0.8)]
@@ -14,12 +15,14 @@ export default function Day2Button() {
             active:translate-x-1
             active:translate-y-1
           "
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <div className="flex items-center gap-1.5 sm:gap-3">
-            <img src="/dice-2.svg" alt="Rulebook Icon" className="w-6 h-6" />
+            <img src="/dice-2.svg" alt="Day 2 Icon" className="w-6 h-6 bg-white rounded" />
             <span className="tracking-wider text-[10px] sm:text-base">
-              Day 2 Registration Coming Soon
+              Day 2 Registration Link
             </span>
           </div>
-        </button>);
+        </a>);
 }
