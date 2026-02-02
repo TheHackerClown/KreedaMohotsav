ca86dc3a913ba1b2bb60769fcf6d1d8cada18ba0
TheHackerClown <dhruvprataps14@gmail.com>
2026-02-02 14:36:32 +0530

    RuleBook Announced

--- components/RuleBook.tsx
+++ components/RuleBook.tsx
@@ -1,10 +1,11 @@
 
 export default function RuleBookButton({ className }: { className?: string }) {
-  return (<button
+  return (<a
+          href="/rules.pdf"
           className={` ${className}
             px-3 sm:px-6 py-1.5 sm:py-3 font-mono text-xs sm:text-base font-bold
             transition-all duration-150 ease-in-out
-            bg-white text-black border-black
+            bg-yellow-300 text-black border-black
             border-2 sm:border-4
             shadow-[3px_3px_0px_0px_rgba(0,0,0,0.8)] sm:shadow-[4px_4px_0px_0px_rgba(0,0,0,0.8)]
             hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,0.8)]
@@ -18,8 +19,8 @@ export default function RuleBookButton({ className }: { className?: string }) {
           <div className="flex items-center gap-1.5 sm:gap-3">
             <img src="/book-half.svg" alt="Rulebook Icon" className="w-6 h-6" />
             <span className="tracking-wider text-[10px] sm:text-base">
-              RuleBook Coming Soon
+              RuleBook (PDF)
             </span>
           </div>
-        </button>);
+        </a>);
 }
