d3931a0c34bf6f7a279cc0aaafe3cc82f801ad90
TheHackerClown <dhruvprataps14@gmail.com>
2026-02-12 13:13:57 +0530

    Leaderboard Declare

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -1,10 +1,11 @@
 
 export default function LeaderboardButton({ className }: { className?: string }) {
   return (<a
+          href="https://kreedascore.vercel.app/"
           className={`${className}
             px-3 sm:px-6 py-1.5 sm:py-3 font-mono text-xs sm:text-base font-bold
             transition-all duration-150 ease-in-out
-            bg-black text-white border-red-950
+            bg-purple-500 text-black border-black
             border-2 sm:border-4
             shadow-[3px_3px_0px_0px_rgba(0,0,0,0.8)] sm:shadow-[4px_4px_0px_0px_rgba(0,0,0,0.8)]
             hover:shadow-[2px_2px_0px_0px_rgba(0,0,0,0.8)]
@@ -14,11 +15,13 @@ export default function LeaderboardButton({ className }: { className?: string })
             active:translate-x-1
             active:translate-y-1
           `}
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <div className="flex items-center gap-1.5 sm:gap-3">
-            <img src="/about.svg" alt="About Us Icon" className="w-6 h-6" />
+            <img src="/robot.svg" alt="Leaderboard Icon" className="w-6 h-6" />
             <span className="tracking-wider text-[10px] sm:text-base">
-              Leaderboard Coming Soon!
+              Live Leaderboard!!! 
             </span>
           </div>
         </a>);
